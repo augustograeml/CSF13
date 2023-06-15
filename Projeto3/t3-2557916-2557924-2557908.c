ee2462ced3d01322736dcb13074ab977db8364e5
Augusto César Graeml <64148703+augustograeml@users.noreply.github.com>
2023-06-15 15:27:57 -0300

    Add files via upload

---
+++ Projeto3/t3-2557916-2557924-2557908.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "gerador_de_testes.h"
+#include "imagem.h"
+#include "trabalho3.h"
+#define BUFFER 3000
+
+unsigned int i, j, k;
+char arquivo[BUFFER];
+void removeFundo(Imagem3C* img, Imagem3C* bg);
+
+int contaVeiculos (Imagem3C* img, Imagem3C* bg, int contagem [N_TIPOS_DE_VEICULOS])
+{
+
+
+removeFundo(img, bg);
+
+
+
+return 0;
+}
+
+void removeFundo(Imagem3C* img, Imagem3C* bg)
+{
+
+    for(i=0; i < img->altura; i ++)
+    {
+        for(j =0; j < img->largura; j++)
+        {
+            for(k = 0; k < 3; k++)
+            {
+
+                if(img->dados[i][j][k] == bg->dados[i][j][k])
+                {
+                  img->dados[i][j][k] = 0x255;
+
+                }
+            }
+        }
+    }
+    sprintf (arquivo, "peloamordedeusfunciona.bmp");
+    salvaImagem3C (img, arquivo);
+
+}
