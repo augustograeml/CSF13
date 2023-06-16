de82bf08860ec76a10aa725bf0d17afb33c0e9af
antoni0net0 <131201632+antoni0net0@users.noreply.github.com>
2023-06-16 18:11:25 -0300

    Update t3-2557916-2557924-2557908.c

--- Projeto3/t3-2557916-2557924-2557908.c
+++ Projeto3/t3-2557916-2557924-2557908.c
@@ -4,7 +4,8 @@
 #include "trabalho3.h"
 #define BUFFER 3000
 
-unsigned int i, j, k;
+unsigned int i, j, k, tolerancia = 30;
+//unsigned char tolerancia = '0';
 char arquivo[BUFFER];
 void removeFundo(Imagem3C* img, Imagem3C* bg);
 
@@ -12,7 +13,7 @@ int contaVeiculos (Imagem3C* img, Imagem3C* bg, int contagem [N_TIPOS_DE_VEICULO
 {
 
 
-removeFundo(img, bg);
+    removeFundo(img, bg);
 
 
 
@@ -29,15 +30,39 @@ void removeFundo(Imagem3C* img, Imagem3C* bg)
             for(k = 0; k < 3; k++)
             {
 
-                if(img->dados[i][j][k] == bg->dados[i][j][k])
+                if(img->dados[k][i][j] <=( bg->dados[k][i][j] + tolerancia) && img->dados[k][i][j] > (bg->dados[k][i][j] - tolerancia))
                 {
-                  img->dados[i][j][k] = 0x255;
+                  img->dados[0][i][j] = 0;
+                  img->dados[1][i][j] = 0;
+                  img->dados[2][i][j] = 0;
+                  
+                  
+                 // img->dados[k][i][j] = 0;
+                }
+            }
+        }
+    }
+    sprintf (arquivo, "t01.bmp");
+    salvaImagem3C (img, arquivo);
+
+
+
+/*
+    for(i=0; i < img->altura; i ++)
+    {
+        for(j =0; j < img->largura; j++)
+        {
+            for(k = 0; k < 3; k++)
+            {
+
+                if(img->dados[k][i][j] <( bg->dados[k][i][j] + tolerancia) && img->dados[k][i][j] > (bg->dados[k][i][j] - tolerancia))
+                {
+                  img->dados[k][i][j] = 0;
 
                 }
             }
         }
     }
-    sprintf (arquivo, "peloamordedeusfunciona.bmp");
-    salvaImagem3C (img, arquivo);
-
+    sprintf (arquivo, "passandopelasegundavez.bmp");
+    salvaImagem3C (img, arquivo);*/
 }
